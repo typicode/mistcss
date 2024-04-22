0fbe51779ac42b7cd946574b4513ee820282f8e9
Andy Wilson <andy.wilson1234@gmail.com>
2024-04-22 11:42:10 +0100

    Add childless html functionality (#45)

---
+++ src/utils.ts
@@ -0,0 +1,20 @@
+const noChildrenTags = new Set([
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr',
+])
+
+export function canTagHaveChildren(tag: string): boolean {
+  return !noChildrenTags.has(tag)
+}
