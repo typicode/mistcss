5acd4b7a1d68d05540504519d73508c4fee7e3cb
typicode <typicode@gmail.com>
2024-05-04 19:24:48 +0200

    feat: vue (#51)

---
+++ src/renderers/vue.test.ts
@@ -0,0 +1,48 @@
+import { expect, it, describe } from 'vitest'
+
+import { Data } from '../parser.js'
+import { render } from './vue.js'
+
+describe('render', () => {
+  it('renders Vue component (full)', () => {
+    const data: Data = {
+      tag: 'div',
+      className: 'foo',
+      attributes: {
+        'data-attr': new Set(['a', 'b']),
+        'data-attr-foo-bar': new Set(['foo-bar']),
+      },
+      booleanAttributes: new Set(['data-is-foo']),
+      properties: new Set(['--prop-foo', '--prop-bar']),
+    }
+
+    const result = render('component', data)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('renders Vue component (minimal)', () => {
+    const data: Data = {
+      tag: 'div',
+      className: 'foo',
+      attributes: {},
+      booleanAttributes: new Set(),
+      properties: new Set(),
+    }
+
+    const result = render('component', data)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('renders Vue component (void element)', () => {
+    const data: Data = {
+      tag: 'hr', // hr is a void element and should not have children
+      className: 'foo',
+      attributes: {},
+      booleanAttributes: new Set(),
+      properties: new Set(),
+    }
+
+    const result = render('component', data)
+    expect(result).toMatchSnapshot()
+  })
+})
